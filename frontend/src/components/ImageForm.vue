dd1bc6ec620f9cbe6f2ad34246b314d9162d337b
gamar <gamarazuaje@gmail.com>
2024-05-10 13:07:18 +0000

    Added validator

--- frontend/src/components/ImageForm.vue
+++ frontend/src/components/ImageForm.vue
@@ -1,11 +1,12 @@
 <template>
     <v-layout class="layout">
-        <form @submit.prevent="submitForm">
+        <v-form ref="form" @submit.prevent="submitForm">
             <v-textarea
                 bg-color="grey-lighten-3"
                 color="secondary"
                 label="Text"
                 v-model="text"
+                :rules="rules"
             ></v-textarea>
             <v-btn color="secondary" @click="submitForm" v-bind:disabled="running">Submit</v-btn>
             <div style="background-color: white;">
@@ -18,7 +19,7 @@
               striped
             ></v-progress-linear>
             </div>
-        </form>
+        </v-form>
 
         
     </v-layout>
@@ -34,6 +35,7 @@ export default {
     const dataStore = useDataStore();
     const text = ref('');
     const imageUrl = ref('');
+    const valid = false;
     
     // Show the task progress
     const taskId = ref(null);
@@ -41,6 +43,19 @@ export default {
     const totalIterations = ref(0); 
     const currentIteration = ref(0);  
     const running = ref(false);
+
+    const form = ref(null);
+    const formValid = computed(() => {
+      if (form.value) {
+        return form.value.validate().then(valid => valid);
+      }
+      return false;
+    });
+
+    const rules = ref([
+      value => !!value || 'Text must not be empty.',
+      value => /[\u3000-\u303F\u3040-\u309F\u30A0-\u30FF\uFF00-\uFFEF\u4E00-\u9FAF]/.test(value) || 'Text must be in Japanese.',
+    ]);
 
     const progress = computed(() => {
       if (totalIterations.value === 0) {
@@ -54,22 +69,16 @@ export default {
         responseType: 'json',
       })
       .then(response => {
-        console.log(response);
         totalIterations.value = response.data.total_iterations;
         currentIteration.value = response.data.current_iteration;
-        console.log(response.data.state)
         if (response.data.state === 'SUCCESS') {
-          // The task is done, clear the interval
           clearInterval(intervalId.value);
-          // Do something with the result
-          console.log(response.data)
 
           axios.get(`/episodewriter/api/get_image/${taskId.value}`, {
             responseType: 'blob',
           })
           .then(response => {
             const url = window.URL.createObjectURL(new Blob([response.data]));
-            console.log(url)
             imageUrl.value = url;
             dataStore.setImage(imageUrl.value);
             dataStore.setCaption(text.value);
@@ -80,30 +89,32 @@ export default {
       });
     };
 
-    
-
     const submitForm = () => {
-      currentIteration.value = 0;
-      totalIterations.value = 0;
-      axios.get('/episodewriter/api/image_generation_task', {
-        params: {
-          prompt: text.value,
-        },
-        responseType: 'json',
-        cache: false,
-      })
-      .then(response => {
-        console.log(response);
-        taskId.value = response.data.task_id;
-        running.value = true;
 
-        setTimeout(() => {
-          intervalId.value = setInterval(checkTaskStatus, 1000);
-        }, 500);
-        
-        // imageUrl.value = URL.createObjectURL(response.data);
-        // dataStore.setImage(imageUrl.value);
-        // dataStore.setCaption(text.value);
+      form.value.validate().then(result => {
+        if (!result.valid) {
+          // Form is not valid, do not submit it
+          return;
+        }
+        // Form is valid, you can submit it      
+        currentIteration.value = 0;
+        totalIterations.value = 0;
+        axios.get('/episodewriter/api/image_generation_task', {
+          params: {
+            prompt: text.value,
+          },
+          responseType: 'json',
+          cache: false,
+        })
+        .then(response => {
+          taskId.value = response.data.task_id;
+          running.value = true;
+
+          setTimeout(() => {
+            intervalId.value = setInterval(checkTaskStatus, 1000);
+          }, 500);
+          
+        });
       });
     };
 
@@ -116,6 +127,10 @@ export default {
       currentIteration,
       running,
       progress,
+      form,
+      formValid,
+      rules, 
+      valid,
     };
   },
 };
